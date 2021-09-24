50d33ef17f6fc531be958581314706d4b9082a20
Esteban <estebanfrancobedoya@gmail.com>
2021-09-24 15:15:41 -0500

    Usando supertest para testear url dentro de bases de datos: 1. creacion de id y token para userOne y testeando login/read profile/ delete account para userOne

--- test/user.test.js
+++ test/user.test.js
@@ -1,17 +1,23 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
 const app = require('../src/app')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const User = require('../src/models/user')
 
+const userOneId = mongoose.Types.ObjectId()
+
 const userOne = {
+	_id: userOneId,
 	name: 'User 1',
 	email:'user1.email@example.com',
-	password:'564What!'
+	password:'564What!',
+	tokens:[{
+		token: jwt.sign({_id:userOneId}, process.env.JWT_SECRET)
+	}]
 }
 beforeEach( async () => {
 	await User.deleteMany()
-	console.log('pase por aqui')
 	await new User(userOne).save()
 })
 
@@ -35,12 +41,42 @@ test('Should signup a new user', async () => {
 	  }).expect(200)
   })
 
-  test('should fail for logint', async () =>{
+  test('should fail for login', async () =>{
 	  await request(app).post('/users/login').send({
 		  email:'emmanuel@gmial.com',
 		  password:'noesElPasswdss'
 	  }).expect('Unable to log in, not email or password found or password incorrect')
   })
+
+  test('should get profile for user', async () =>{
+	  await request(app)
+	  	.get('/users/me')
+		.set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+		.send()
+		.expect(200)
+  })
+
+  test('should fail getting the profile for unauthenticated user', async () =>{
+	  await request(app)
+	  	.get('/users/me')
+		.send()
+		.expect(400)
+  })
+
+  test('Should delete account for user', async () =>{
+	  await request(app)
+	  	.delete('/users/me')
+		.set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+		.send()
+		.expect(200)
+  })
+
+  test('fail delete account for unauthenticated user', async () =>{
+	await request(app)
+		.delete('/users/me')
+	    .send()
+	    .expect(400)
+})
 
 // afterAll(() => {
 //     mongoose.connection.close();
