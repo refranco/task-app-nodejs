4a59cb5e99bc1cc4b0f3f16eaf561024bf6b7f48
Esteban <estebanfrancobedoya@gmail.com>
2021-09-25 11:20:15 -0500

    agregando la funcionalidad de mock npm libraries dentro de test

---
+++ test/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,12 @@
+module.exports = {
+	setApiKey() {
+
+	},
+	async send(){
+
+	}
+}
+
+/* queremos emular sgMail, libreria exportada
+ en emails/account.js, para esto emulamos los
+  métodos o funciones que esta llama */
